e8a37ab4b3bb75c63fca9e11e05ab50d6da53c8a
Majda Ivic <majdaivic@yahoo.com>
2025-09-29 14:09:05 +0200

    new update

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,20 +1,33 @@
-terraform { 
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 3.0"
-        }
+
+
+terraform {
+  required_providers {
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~> 3.0"
     }
+  }
 }
+
+
 
 provider "azurerm" {
     features {}
-
 }
 
-resource "azurerm_resource_group" "rg" {
-  
-  name = "my_r_gr"
+resource "azurerm_resource_group" "rg1" {
+  name = "resource_nr1"
   location = "westeurope"
+  tags = { 
+    name = "nr1"
+   }
+  
+}
 
+resource "azurerm_resource_group" "rg2" {
+    name = "resourcenr2"
+  location = "westeurope"
+  tags = { 
+    name = "nr2"
+   }
 }
