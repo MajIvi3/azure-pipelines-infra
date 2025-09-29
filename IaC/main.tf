e650ae45e87e58bc848fa6dec6034d4bc60d0884
Majda Ivic <majdaivic@yahoo.com>
2025-09-29 12:36:06 +0200

    Initial commit with terraform files

---
+++ IaC/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "resource_nr1"
+  location = "westeurope"
+  
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "resourcenr2"
+  location = "westeurope"
+}
