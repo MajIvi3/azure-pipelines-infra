a0fd38a3c228bf2756558e631aa34d558ca30d99
Majda Ivic <majdaivic@yahoo.com>
2025-09-29 12:40:11 +0200

    add basic html file

--- IaC/main.tf
+++ IaC/main.tf
@@ -14,10 +14,16 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name = "resource_nr1"
   location = "westeurope"
+  tags = { 
+    name = "nr1"
+   }
   
 }
 
 resource "azurerm_resource_group" "rg" {
     name = "resourcenr2"
   location = "westeurope"
+  tags = { 
+    name = "nr2"
+   }
 }
